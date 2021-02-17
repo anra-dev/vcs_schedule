aaccb7feeccd3fedb800be375be52700710834ad
romanandreev <anra@bk.ru>
2021-02-17 22:47:45 +0300

    Доделана валидация конференций и создана валидация комнат

--- vcs_site/main/forms.py
+++ vcs_site/main/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model
 import datetime
 
-from .models import Event, Conference, Booking
-from .services import check_ability_to_create
+from .models import Event, Conference, Booking, Room
+from .services import check_ability_to_create_conf, check_room_is_free
 
 User = get_user_model()
 
@@ -60,15 +60,20 @@ class ConferenceAddForm(forms.ModelForm):
         quota = self.cleaned_data['quota']
         application = self.cleaned_data['application']
         date = self.cleaned_data['date']
+        type = self.cleaned_data['type']
+        conf_id = self.instance.id  # почему не падает при создании конфы если равно None?
+
         # Начало должно быть раньше конца
         if time_start >= time_end:
             self.add_error('time_start', 'Время начала не может совпадать или быть позже окончания')
             self.add_error('time_end', 'Время окончания не может совпадать или быть раньше начала')
-
-        if not check_ability_to_create(application=application, date=date, time_start=time_start, time_end=time_end, quote=quota):
-            self.add_error('quota', 'Превышено количество пользователей (учитываются все уже назначенный конференции).'
-                                    'Измените количество участников или время проведения')
-
+        # Проверка на наличие свободных лицензий
+        if type == 'local':
+            if not check_ability_to_create_conf(conf_id=conf_id, application=application, date=date, time_start=time_start,
+                                                time_end=time_end, quote=quota):
+                self.add_error('quota', 'Превышено количество пользователей (учитываются все уже назначенный '
+                                        'конференции). Измените количество участников или время проведения')
+        return self.cleaned_data
 
     class Meta:
         model = Conference
@@ -93,20 +98,52 @@ class BookingAddForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['event'].widget.attrs['readonly'] = True
+        self.fields['date'].widget.attrs['readonly'] = True
         self.fields['time_start'].label = 'Время начала'
         self.fields['time_end'].label = 'Время окончания'
 
     time_start = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}))
     time_end = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}))
 
+    def clean(self):
+        """Валидация формы"""
+        time_start = self.cleaned_data['time_start']
+        time_end = self.cleaned_data['time_end']
+        quota = self.cleaned_data['quota']
+        room = self.cleaned_data['room']
+        date = self.cleaned_data['date']
+        booking_id = self.instance.id
+
+        # Начало должно быть раньше конца
+        if time_start >= time_end:
+            self.add_error('time_start', 'Время начала не может совпадать или быть позже окончания')
+            self.add_error('time_end', 'Время окончания не может совпадать или быть раньше начала')
+
+        # Время не должно быть занято кем то ранее
+        if check_room_is_free(booking_id, room, date, time_start, time_end):
+            self.add_error('time_start', 'Время занято другим мероприятием')
+            self.add_error('time_end', 'Время занято другим мероприятием')
+
+        # Количество участников не должно превышать вместимость комнаты
+        if quota > room.quota:
+            self.add_error('quota', f'Количество участников превышает вместимость "{room.__str__()}"')
+        return self.cleaned_data
+
     class Meta:
         model = Booking
         fields = (
+            'event',
+            'date',
             'room',
             'quota',
             'time_start',
             'time_end',
         )
+        widgets = {
+            'event': forms.HiddenInput(),
+            'date': forms.HiddenInput(),
+        }
 
 
 class LoginForm(forms.ModelForm):
--- vcs_site/main/services.py
+++ vcs_site/main/services.py
@@ -1,35 +1,42 @@
-from django.db.models import Q, Sum
-from .models import Conference, Application
+from django.db.models import Sum, Q
+from .models import Conference, Application, Booking, Room
 import datetime
 
 
-def check_ability_to_create(application, date, time_start, time_end, quote):
+def check_ability_to_create_conf(conf_id, application, date, time_start, time_end, quote):
     """
     Проверяет возможность проведения конференции в заданный интервал времени и возвращает True
     если количество занятых лицензий на протежении всего промежутка времени
     между time_start и time_end в сумме с запрашиваемой квотой не превышает базовое
-    количество лицензий  приложения application
+    количество лицензий  приложения application. При редактировании используется conf_id.
     """
-    print('На входе', quote)
-    # Создаем QuerySet - это мероприятия которые начинаются во время планируемого мероприятия.
-    query_set = Conference.objects.filter(date=date, application=application,
-                                          time_start__gte=time_start, time_start__lt=time_end)
+    # Создаем QuerySet - это мероприятия которые начинаются во время планируемого мероприятия. Исключаем текущее.
+    query_set = Conference.objects.filter(date=date, application=application, type='local',
+                                          time_start__gte=time_start, time_start__lt=time_end).exclude(pk=conf_id)
     # Квота изменяется в момент начала мероприятий. Выбираем точки
     points = []
     for obj in query_set:
         points.append(obj.time_start)
-    # Сичтаем квоту в точках
+    # Считаем квоту в точках
     qty_lic_in_pont = []
     for point in set(points):
-        qty_lic = Conference.objects.filter(date=date, application=application,
-                                            time_start__lte=point, time_end__gt=point).aggregate(Sum('quota'))
+        qty_lic = Conference.objects.filter(date=date, application=application, type='local', time_start__lte=point,
+                                            time_end__gt=point).exclude(pk=conf_id).aggregate(Sum('quota'))
         qty_lic_in_pont.append(qty_lic['quota__sum'])
-    print('Результат проверки', qty_lic_in_pont)
-    # Возвращаем максимум
     lic = Application.objects.get(name=application).quota
     if qty_lic_in_pont:
         quote += max(qty_lic_in_pont)
-    print('На выходе', quote)
     return lic >= quote
 
+
+def check_room_is_free(booking_id, room, date, time_start, time_end):
+    """
+    Проверяет свободна ли комната room в день data d промежутке времени между time_start и time_end.
+    Если свободна то возвращает True иначе False.
+    """
+    booking_on_date = Booking.objects.filter(
+        Q(date=date, room=room), Q(time_start__gte=time_start, time_start__lt=time_end) |
+        Q(time_start__lte=time_start, time_end__gt=time_start)
+    ).exclude(pk=booking_id)
+    return bool(booking_on_date)
 
