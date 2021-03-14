1fcadc25da84ac393e1b104fa66cf607b77a1e69
romanandreev <anra@bk.ru>
2021-03-14 20:59:02 +0300

    Подключен rich-text редактор CKEditor для страниц Помощи

--- vcs_site/help/admin.py
+++ vcs_site/help/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 
 from .models import Page, Section
 
 
+class PageAdminForm(forms.ModelForm):
+    body_text = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model =Page
+        fields = '__all__'
+
+
 class PageAdmin(admin.ModelAdmin):
+    form = PageAdminForm
     list_display = ('name', 'title', 'update_date')
     ordering = ('name',)
     search_fields = ('title', 'body_text')
--- vcs_site/vcs_site/settings.py
+++ vcs_site/vcs_site/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'schedule',
     'accounts',
     'dispatch',
     'help',
+
     'crispy_forms',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
